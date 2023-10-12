33979b3975bdb317119c2bf3fec98581265c5a8b
danilo_cj <danilo_cj@DESKTOP-BT8DAVV>
2023-10-12 03:15:45 +0300

    Add project files.

---
+++ HW5T2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Dictionary<uint, string> personDictionary = new Dictionary<uint, string>();
+
+        
+        for (int i = 0; i < 7; i++)
+        {
+            Console.Write("Enter ID: ");
+            if (uint.TryParse(Console.ReadLine(), out uint id))
+            {
+                Console.Write("Enter Name: ");
+                string name = Console.ReadLine();
+                personDictionary[id] = name;
+            }
+            else
+            {
+                Console.WriteLine("Invalid ID. Please enter a valid positive number.");
+                i--; 
+            }
+        }
+
+        Console.Write("Enter ID to find Name: ");
+        if (uint.TryParse(Console.ReadLine(), out uint searchID))
+        {
+            if (personDictionary.ContainsKey(searchID))
+            {
+                string foundName = personDictionary[searchID];
+                Console.WriteLine($"Name for ID {searchID}: {foundName}");
+            }
+            else
+            {
+                Console.WriteLine($"ID {searchID} not found in the Dictionary.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid ID. Please enter a valid positive number.");
+        }
+    }
+}
